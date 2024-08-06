2637443cc91cb5ad4280ea124345624ca07d7bcb
kyriediculous <vergauwennico@gmail.com>
2024-08-06 04:32:30 +0200

    port flag

--- openai-api.go
+++ openai-api.go
@@ -10,13 +10,14 @@ import (
 
 func main() {
 	gatewayURL := flag.String("gateway", "http://your-api-host", "The URL of the gateway API")
+	port := flag.String("port", "8080", "The port to run the server on")
 	flag.Parse()
 	gateway := middleware.NewGateway(*gatewayURL)
 	server, err := server.NewServer(gateway)
 	if err != nil {
 		log.Fatalf("Failed to create server: %v", err)
 	}
-	if err := server.Start("8080"); err != nil {
+	if err := server.Start(*port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
